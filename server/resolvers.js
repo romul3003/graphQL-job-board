78644f3ff9fb64ff896b14249eb93452be730e4a
romka <romanzakrevskyi@romka.local>
2024-06-14 23:53:35 +0300

    Add schema

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,19 @@
+import { getCompany } from './db/companies.js'
+import { getJobs } from './db/jobs.js'
+
 export const resolvers = {
   Query: {
-    greeting: () => 'Hello world!',
+    jobs: async () => getJobs(),
   },
+
+  Job: {
+    // add company from db to response according to schema
+    company: job => getCompany(job.companyId),
+    // add date from db to response
+    date: job => toIsoDate(job.createdAt),
+  },
+}
+
+function toIsoDate(value) {
+  return value.slice(0, 'yyyy-mm-dd'.length)
 }
