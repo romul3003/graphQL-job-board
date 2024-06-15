889b82c73cbabb905de694ffea0efabb03c754db
romka <romul3003@gmail.com>
2024-06-15 22:24:18 +0300

    Add error handling

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,28 @@
+import { GraphQLError } from 'graphql'
+
 import { getCompany } from './db/companies.js'
 import { getJob, getJobs, getJobsByCompany } from './db/jobs.js'
 
 export const resolvers = {
   Query: {
-    company: (_root, { id }) => getCompany(id),
-    job: (_root, { id }) => getJob(id),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id)
+
+      if (!company) {
+        throw notFoundError(`No Company found with id ${id}`)
+      }
+
+      return company
+    },
+    job: async (_root, { id }) => {
+      const job = await getJob(id)
+
+      if (!job) {
+        throw notFoundError(`No Job found with id ${id}`)
+      }
+
+      return job
+    },
     jobs: () => getJobs(),
   },
 
@@ -18,6 +36,12 @@ export const resolvers = {
     // add date from db to response
     date: job => toIsoDate(job.createdAt),
   },
+}
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'NOT_FOUND' },
+  })
 }
 
 function toIsoDate(value) {
